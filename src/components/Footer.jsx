b54f6c6417aea35161e1b222a87d9f4e2aa1dcf2
unknown <kelvinoye@gmail.com>
2024-05-28 07:06:51 +0100

    Add styling to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,17 @@
 import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { BsTwitterX } from "react-icons/bs";
 import logo from "../assets/Aisling (1).jpg";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className=" bg-white py-10 flex flex-col items-center gap-3">
       <div className="flex justify-center items-center w-full max-w-md gap-5 ">
         <div className="footer__img__container  bg-pink-500 ">
-          <img src={logo} alt="aislings" className="footer__img" />
+          <Link to={"/"}>
+            {" "}
+            <img src={logo} alt="aislings" className="footer__img" />
+          </Link>
         </div>
         <div className="flex  gap-2 ">
           <a href="">
@@ -28,7 +32,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="text-center">
-        Copyright &copy; Aislings {new Date().getFullYear()}
+        Copyright &copy; Aislings {new Date().getFullYear()} All Rights Reserved
       </p>
     </footer>
   );
